f6604948117aa82a758864cc20dbab16afb40e0c
Luis Checa <luis@checa.xyz>
2019-03-06 01:12:27 +0000

    "Added BlazorExtensions.Logging"

--- MyAppComponent.cs
+++ MyAppComponent.cs
@@ -1,8 +1,12 @@
 ﻿using BlazorRedux;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorStandalone
 {
     public class MyAppComponent : ReduxComponent<MyState, IAction>
     {
+        [Inject]
+        protected ILogger<MyAppComponent> Logger {get;set;}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using Blazor.Extensions.Logging;
 using BlazorRedux;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorStandalone
 {
@@ -8,6 +10,7 @@ namespace BlazorStandalone
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(builder => builder.AddBrowserConsole().SetMinimumLevel(LogLevel.Trace));
             services.AddReduxStore<MyState, IAction>(new MyState(), Reducers.RootReducer, options=>options.GetLocation=state=>state.Location);
         }
 
